235553f56fc88f0a7740642632d86a94672ddfcf
Ritesh <codeshorts007@gmail.com>
2023-08-11 18:26:56 +0530

    style- home page banner design

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./Home.scss";
+import Banner from "../../components/Banner/Banner";
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <Banner />
+    </div>
+  );
 }
 
 export default Home;
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,24 @@
 import React from "react";
 import "./Banner.scss";
+import { Link } from "react-router-dom";
 function Banner() {
-  return <div className="banner"></div>;
+  return (
+    <div className="banner">
+      <img
+        src="https://gatsby-ecommerce-theme.netlify.app/banner1.png"
+        alt=""
+      />
+      <div className="banner-info">
+        <h1>
+          Essentials for <br></br> a cold winter
+        </h1>
+        <p>Discover Autumn Winter 2021</p>
+        <Link to={"/products"}>
+          <button>SHOP NOW</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
