27a91482057d3f2f1ddb2cdd2635e0bc7c3a63ee
Ritesh <codeshorts007@gmail.com>
2023-08-11 19:33:53 +0530

    style- design category section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Home.scss";
 import Banner from "../../components/Banner/Banner";
+import Categories from "../../components/Categories/Categories";
 function Home() {
   return (
     <div className="home">
       <Banner />
+      <Categories />
     </div>
   );
 }
--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,7 +1,54 @@
 import React from "react";
 import "./Categories.scss";
+import { Link } from "react-router-dom";
 function Categories() {
-  return <div className="categories"></div>;
+  const data = [
+    {
+      id: 1,
+      img: "https://gatsby-ecommerce-theme.netlify.app/collections/collection1.png",
+      category: "Men",
+    },
+    {
+      id: 2,
+      img: "https://gatsby-ecommerce-theme.netlify.app/collections/collection2.png",
+      category: "Women",
+    },
+    {
+      id: 3,
+      img: "https://gatsby-ecommerce-theme.netlify.app/collections/collection3.png",
+      category: "Accessories",
+    },
+    {
+      id: 4,
+      img: "https://gatsby-ecommerce-theme.netlify.app/collections/collection4.png",
+      category: "Simple Cotton",
+    },
+  ];
+  return (
+    <div className="categories">
+      <div className="container">
+        <h3>New Collection</h3>
+        <div className="wrapper">
+          {data?.map((item) => {
+            return (
+              <Link
+                className="card"
+                key={item.id}
+                to={`/products/${item.category}`}
+              >
+                <img src={item.img} alt={item.category} />
+
+                <div className="cate">
+                  <h3>{item.category}</h3>
+                  <button>SHOP NOW</button>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Categories;
