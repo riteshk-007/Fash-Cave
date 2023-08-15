473e854f415c998b66016a8a029fa177ff2b072b
Ritesh <codeshorts007@gmail.com>
2023-08-15 16:46:51 +0530

    style- design 2nd hero banner

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,12 +3,14 @@ import "./Home.scss";
 import Banner from "../../components/Banner/Banner";
 import Categories from "../../components/Categories/Categories";
 import NewArrivals from "../../components/NewArrivals/NewArrivals";
+import HeroModel from "../../components/HeroModel/HeroModel";
 function Home() {
   return (
     <div className="home">
       <Banner />
       <Categories />
       <NewArrivals />
+      <HeroModel />
     </div>
   );
 }
--- src/components/HeroModel/HeroModel.js
+++ src/components/HeroModel/HeroModel.js
@@ -1,7 +1,22 @@
 import React from "react";
 import "./HeroModel.scss";
+import { Link } from "react-router-dom";
 function HeroModel() {
-  return <div className="heroModel"></div>;
+  return (
+    <div className="heroModel">
+      <h1 className="main-heading">
+        -50% off<br></br> All Essentials
+      </h1>
+      <span>
+        <Link className="link style" to={"/"}>
+          WOMEN
+        </Link>
+        <Link className="link style" to={"/"}>
+          MEN
+        </Link>
+      </span>
+    </div>
+  );
 }
 
 export default HeroModel;
