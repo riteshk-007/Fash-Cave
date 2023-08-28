d5c2803ea4cf543d83223ba9a2e346dd1f7db791
Ritesh <codeshorts007@gmail.com>
2023-08-28 16:12:40 +0530

    fix- env file error

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,1 +1,3 @@
-export const API_URL = "http://localhost:1337" || "http://127.0.0.1:1337";
+export const STRAPI_API_TOKEN = process.env.REACT_APP_STARPI_API_TOKEN;
+export const API_URL =
+  process.env.REACT_APP_STARPI_API_URL || "http://127.0.0.1:1337";
